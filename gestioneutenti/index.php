1b568ea8b73ea9a1c0588297c587fe8c60c4a79b
cescofrann <75941302+cescofrann@users.noreply.github.com>
2023-03-22 18:13:47 +0100

    v0.05

--- gestioneutenti/index.php
+++ gestioneutenti/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Gestione Bici</title>
+	<title>Gestione Utente</title>
 
 	<link rel="stylesheet" href="main.css">
 </head>
